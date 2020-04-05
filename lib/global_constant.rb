d90690fd8cd040fa2e8dfb9380a38758c2fe3851
Chetandeep Singh <chetandeep.singh@caroobi.com>
2020-04-06 04:38:56 +0530

    add prod constants file

--- lib/global_constant.rb
+++ lib/global_constant.rb
@@ -1,5 +1,10 @@
 module GlobalConstant
-  env_constants = YAML.load_file(Rails.root.to_s + '/config/constants.yml')['constants']
+  if RAils.env.production?
+    env_constants = YAML.load_file(Rails.root.to_s + '/config/prod_constants.yml')['constants']
+  else
+    env_constants = YAML.load_file(Rails.root.to_s + '/config/constants.yml')['constants']
+  end
+
 
   APP_URL = env_constants['app']['url']
 end
